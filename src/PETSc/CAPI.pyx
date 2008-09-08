611522cb4caa737aa472192e9757fa8ee68e326b
Lisandro Dalcin <dalcinl@gmail.com>
2008-09-08 20:45:00 -0300

    fix test case, gc is not working!

---
+++ src/PETSc/CAPI.pyx
@@ -0,0 +1,181 @@
+#---------------------------------------------------------------------
+
+cdef extern from "petsc.h":
+    int PyPetscINCREF"PetscObjectReference"(PetscObject)
+
+cdef int addref(void *o) except -1:
+    if o == NULL: return 0
+    CHKERR( PyPetscINCREF(<PetscObject>o) )
+    return 0
+
+#---------------------------------------------------------------------
+
+# -- Error --
+
+cdef api int PyPetscError_Set(int ierr):
+    return SETERR(ierr)
+
+# -- Comm --
+
+cdef api object PyPetscComm_New(MPI_Comm arg):
+    cdef Comm retv = Comm()
+    retv.comm = arg
+    return retv
+
+cdef api MPI_Comm PyPetscComm_Get(object arg) except *:
+    cdef MPI_Comm retv = MPI_COMM_NULL
+    cdef Comm ob = <Comm?> arg
+    retv = ob.comm
+    return retv
+
+cdef api MPI_Comm* PyPetscComm_GetPtr(object arg) except NULL:
+    cdef MPI_Comm *retv = NULL
+    cdef Comm ob = <Comm?> arg
+    retv = &ob.comm
+    return retv
+
+# -- Object --
+
+cdef api PetscObject PyPetscObject_Get(object arg) except ? NULL:
+    cdef PetscObject retv = NULL
+    cdef Object ob = <Object?> arg
+    retv = ob.obj[0]
+    return retv
+
+cdef api PetscObject* PyPetscObject_GetPtr(object arg) except NULL:
+    cdef PetscObject *retv = NULL
+    cdef Object ob = <Object?> arg
+    retv = ob.obj
+    return retv
+
+# -- Viewer --
+
+cdef api object PyPetscViewer_New(PetscViewer arg):
+    cdef Viewer retv = Viewer()
+    addref(arg); retv.vwr = arg
+    return retv
+
+cdef api PetscViewer PyPetscViewer_Get(object arg) except ? NULL:
+    cdef PetscViewer retv = NULL
+    cdef Viewer ob = <Viewer?> arg
+    retv = ob.vwr
+    return retv
+
+# -- IS --
+
+cdef api object PyPetscIS_New(PetscIS arg):
+    cdef IS retv = IS()
+    addref(arg); retv.iset = arg
+    return retv
+
+cdef api PetscIS PyPetscIS_Get(object arg) except? NULL:
+    cdef PetscIS retv = NULL
+    cdef IS ob = <IS?> arg
+    retv = ob.iset
+    return retv
+
+# -- LGMap --
+
+cdef api object PyPetscLGMap_New(PetscLGMap arg):
+    cdef LGMap retv = LGMap()
+    addref(arg); retv.lgm = arg
+    return retv
+
+cdef api PetscLGMap PyPetscLGMap_Get(object arg) except ? NULL:
+    cdef PetscLGMap retv = NULL
+    cdef LGMap ob = <LGMap?> arg
+    retv = ob.lgm
+    return retv
+
+# -- Vec --
+
+cdef api object PyPetscVec_New(PetscVec arg):
+    cdef Vec retv = Vec()
+    addref(arg); retv.vec = arg
+    return retv
+
+cdef api PetscVec PyPetscVec_Get(object arg) except ? NULL:
+    cdef PetscVec retv = NULL
+    cdef Vec ob = <Vec?> arg
+    retv = ob.vec
+    return retv
+
+# -- Mat --
+
+cdef api object PyPetscMat_New(PetscMat arg):
+    cdef Mat retv = Mat()
+    addref(arg); retv.mat = arg
+    return retv
+
+cdef api PetscMat PyPetscMat_Get(object arg) except ? NULL:
+    cdef PetscMat retv = NULL
+    cdef Mat ob = <Mat?> arg
+    retv = ob.mat
+    return retv
+
+# -- PC --
+
+cdef api object PyPetscPC_New(PetscPC arg):
+    cdef PC retv = PC()
+    addref(arg); retv.pc = arg
+    return retv
+
+cdef api PetscPC PyPetscPC_Get(object arg) except ? NULL:
+    cdef PetscPC retv = NULL
+    cdef PC ob = <PC?> arg
+    retv = ob.pc
+    return retv
+
+# -- KSP --
+
+cdef api object PyPetscKSP_New(PetscKSP arg):
+    cdef KSP retv = KSP()
+    addref(arg); retv.ksp = arg
+    return retv
+
+cdef api PetscKSP PyPetscKSP_Get(object arg) except ? NULL:
+    cdef PetscKSP retv = NULL
+    cdef KSP ob = <KSP?> arg
+    retv = ob.ksp
+    return retv
+
+# -- SNES --
+
+cdef api object PyPetscSNES_New(PetscSNES arg):
+    cdef SNES retv = SNES()
+    addref(arg); retv.snes = arg
+    return retv
+
+cdef api PetscSNES PyPetscSNES_Get(object arg) except ? NULL:
+    cdef PetscSNES retv = NULL
+    cdef SNES ob = <SNES?> arg
+    retv = ob.snes
+    return retv
+
+# -- TS --
+
+cdef api object PyPetscTS_New(PetscTS arg):
+    cdef TS retv = TS()
+    addref(arg); retv.ts = arg
+    return retv
+
+cdef api PetscTS PyPetscTS_Get(object arg) except ? NULL:
+    cdef PetscTS retv = NULL
+    cdef TS ob = <TS?> arg
+    retv = ob.ts
+    return retv
+
+# -- AO --
+
+cdef api object PyPetscAO_New(PetscAO arg):
+    cdef AO retv = AO()
+    addref(arg); retv.ao = arg
+    return retv
+
+cdef api PetscAO PyPetscAO_Get(object arg) except ? NULL:
+    cdef PetscAO retv = NULL
+    cdef AO ob = <AO?> arg
+    retv = ob.ao
+    return retv
+
+#---------------------------------------------------------------------
---
+++ test/test_is.py
@@ -0,0 +1,133 @@
+from petsc4py import PETSc
+import unittest
+import random
+
+# --------------------------------------------------------------------
+
+class TestISBase(object):
+
+    TYPE = None
+
+    def tearDown(self):
+        self.iset = None
+
+    def testGetType(self):
+        istype = self.iset.getType()
+        self.assertEqual(istype, self.TYPE)
+
+    def testGetSize(self):
+        lsize = self.iset.getLocalSize()
+        gsize = self.iset.getSize()
+        self.assertTrue(lsize <= gsize)
+
+    def testDuplicate(self):
+        iset = self.iset.duplicate()
+        self.assertTrue(self.iset.equal(iset))
+        del iset
+
+    def testEqual(self):
+        self.assertTrue(self.iset.equal(self.iset))
+        iset = self.iset.duplicate()
+        self.assertTrue(self.iset.equal(iset))
+        del iset
+
+    def testSort(self):
+        self.iset.sort()
+        self.assertTrue(self.iset.isSorted())
+
+    def testDifference(self):
+        iset = self.iset.difference(self.iset)
+        self.assertEqual(iset.getLocalSize(), 0)
+        del iset
+
+    def testSum(self):
+        if self.iset.getComm().getSize() > 1:
+            return
+        self.iset.sort()
+        iset = self.iset.duplicate()
+        iset.sum(self.iset)
+        self.assertTrue(self.iset.equal(iset))
+        del iset
+
+    def testExpand(self):
+        iset = self.iset.expand(self.iset)
+        if self.iset.type == iset.type:
+            self.assertTrue(self.iset.equal(iset))
+        del iset
+
+    def testProperties(self):
+        proplist = ['sizes', 'size', 'local_size',
+                    'permutation', 'identity', 'sorted']
+        for prop in proplist:
+            self.assertTrue(hasattr(self.iset, prop))
+
+
+# --------------------------------------------------------------------
+
+class TestISGeneral(TestISBase, unittest.TestCase):
+
+    TYPE = PETSc.IS.Type.GENERAL
+
+    def setUp(self):
+        self.idx = list(range(10))
+        random.shuffle(self.idx)
+        self.iset = PETSc.IS().createGeneral(self.idx)
+
+    def testGetIndices(self):
+        idx = self.iset.getIndices()
+        self.assertEqual(self.idx, list(idx))
+
+
+class TestISStride(TestISBase, unittest.TestCase):
+
+    TYPE = PETSc.IS.Type.STRIDE
+
+    def setUp(self):
+        self.iset = PETSc.IS().createStride(10)
+
+    def testGetIndices(self):
+        self.assertEqual(list(self.iset.getIndices()),
+                         list(range(self.iset.getLocalSize())))
+
+    def testToGeneral(self):
+        self.iset.toGeneral()
+        self.assertEqual(self.iset.getType(), PETSc.IS.Type.GENERAL)
+
+
+class TestISBlock(TestISBase, unittest.TestCase):
+
+    TYPE = PETSc.IS.Type.BLOCK
+
+    def setUp(self):
+        self.bsize = 3
+        self.index = list(range(0,10,2))
+        random.shuffle(self.index)
+        self.iset = PETSc.IS().createBlock(self.bsize, self.index)
+        self.assertEqual(self.iset.getType(), PETSc.IS.Type.BLOCK)
+
+    def testGetSize(self):
+        lsize = self.iset.getLocalSize()
+        self.assertEqual(lsize/self.bsize, len(self.index))
+
+    def testGetBlockSize(self):
+        bs = self.iset.getBlockSize()
+        self.assertEqual(bs, self.bsize)
+
+    def testGetIndicesBlock(self):
+        index = list(self.iset.getIndicesBlock())
+        self.assertEqual(index, self.index)
+
+    def testGetIndices(self):
+        bs = self.bsize
+        idx = []
+        for i in self.iset.getIndicesBlock():
+            for j in range(bs):
+                idx.append(i+j)
+        index = list(self.iset.getIndices())
+        self.assertEqual(index, idx)
+
+
+# --------------------------------------------------------------------
+
+if __name__ == '__main__':
+    unittest.main()
